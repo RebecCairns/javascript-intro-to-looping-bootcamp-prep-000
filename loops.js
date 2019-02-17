4d147351c6733ca87bab12958fbcd80a375ecc00
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-17 04:03:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(arr){
   for(let i = 10; i < 35 ; i++){
     if(i === 10){
-      arr[10] = "I am 1 strange loop.";
+      arr[11] = "I am 1 strange loop.";
     }
-    if(i!== 10){
+    if(i!== 11){
       arr[i] = "I am " + (i - 10) + " strange loops.";
     }
   }
